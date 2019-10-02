eb251bafca60d91742dee0d5a97775bd8540962c
Amber Taranto <ambzzz@gmail.com>
2019-10-01 21:28:07 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,61 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+
+  get '/articles/new' do
+
+    erb :new
+  end
+
+  get '/articles/:id' do
+
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+
+  post '/articles' do
+    article = Article.new #ruby object
+
+    article.title = params[:title]
+    article.content = params[:content]
+
+    article.save
+
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles' do
+
+    @articles = Article.all
+    erb :index      
+  end
+
+  get '/articles/:id/edit' do
+
+    @article = Article.find(params[:id])
+    
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    
+    article = Article.find(params[:id])
+    article.title = params[:title]
+    article.content = params[:content]
+
+    article.save
+
+    redirect "/articles/#{article.id}"
+  end
+
+  delete '/articles/:id' do
+    Article.delete(params[:id])
+    redirect "/articles"
+  end
+
+
+
+
 end
